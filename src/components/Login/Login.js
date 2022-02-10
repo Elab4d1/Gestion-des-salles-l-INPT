977ffe058e0d0b6b9b001d4aa5bb649cebfba8a6
Hassan Al Meftah <85963840+Elab4d1@users.noreply.github.com>
2022-02-10 16:12:19 +0100

    Add files via upload

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,19 @@ import logo from "../../assets/logo.png";
 import user from "../../assets/user.png";
 import password from "../../assets/password.png";
 import "./login.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [pw, setPw] = useState("");
+  const [us, setUs] = useState("");
+  const [isError, setIsError] = useState(false);
+  const checkForm = (e) => {
+    if (!pw.length || !us.length) {
+      e.preventDefault();
+      setIsError(true);
+    }
+  };
+
   return (
     <div className="flex">
       <div className="classe-image">
@@ -17,18 +28,46 @@ const Login = () => {
         <form>
           <div className="username">
             <img src={user} alt="" />
-            <input type="text" name="username" placeholder="Username" />
+            <input
+              type="text"
+              name="username"
+              value={us}
+              onChange={(e) => setUs((user) => e.target.value)}
+              placeholder="Username"
+            />
           </div>
           <div className="password">
             <img src={password} alt="" />
-            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              value={pw}
+              onChange={(e) => setPw((ps) => e.target.value)}
+              placeholder="Password"
+            />
           </div>
+          {isError && (
+            <div className="error">
+              {!pw.length && !us.length
+                ? "Veuillez remplir le nom d'utilisateur et le mot de passe"
+                : !pw.length
+                ? "Veuillez remplir le mot de passe"
+                : !us.length
+                ? "Veuillez remplir le nom d'utilisateur"
+                : ""}
+            </div>
+          )}
           <div className="remember">
             <input type="checkbox" id="remember" />
             <label htmlFor="remember">Remember me</label>
           </div>
           <div className="center">
-            <input className="submit" type="submit" value="Login" />
+            <input
+              className="submit"
+              onClick={checkForm}
+              type="submit"
+              value="Login"
+            />
           </div>
         </form>
       </div>
